612bf46de1ccf5a664c7600d54f30c6379087884
Yuan <yuan@yuan25.com>
2015-05-19 02:17:46 -0700

    Go

---
+++ build_run.sh
@@ -0,0 +1,16 @@
+echo -e "\033[31m---------------------------\033[0m"
+echo -e "\033[31m Try to build project\033[0m"
+echo -e "\033[31m---------------------------\033[0m"
+
+GRADLE_PATH="$PWD/gradle-2.4"
+PATH="$GRADLE_PATH/bin:$PATH"
+gradle build
+
+echo -e "\033[31m---------------------------\033[0m"
+echo -e "\033[31m Run project\033[0m"
+echo -e "\033[31m---------------------------\033[0m"
+
+sudo service mongodb restart
+sudo service nginx restart
+
+java -jar ./build/libs/*.jar
