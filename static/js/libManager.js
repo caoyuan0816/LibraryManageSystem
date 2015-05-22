fb680d413aff299956df4b1c14deb218828c146b
Yuan <yuan@yuan25.com>
2015-05-22 11:21:30 -0700

    12

--- static/js/libManager.js
+++ static/js/libManager.js
@@ -240,6 +240,7 @@ $(function () {
     $('#forgetBtn').on('click', function (event) {
         var Data = {};
         Data['email'] = $('#email')[0].value;
+        console.log(Data);
         $.post('/api/forget-password/', Data, function (data) {
             if (data.status) {
 
