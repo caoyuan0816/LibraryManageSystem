5138bc52e237bdba4257cd4ebdcb4da2ff93ed28
superpig <940578704@qq.com>
2015-05-28 01:12:23 +0800

    upload book page

--- src/main/java/hello/model/Book.java
+++ src/main/java/hello/model/Book.java
@@ -5,10 +5,106 @@ package hello.model;
  */
 public class Book {
 
-    private String title;
+    private String bookID;
     private String ISBN;
+    private String bookName;
+    private Author author;
+    private String translator;
+    private String press;
+    private double price;
+    private String money;
+    private int number;
+    public boolean equals(Book book){
+        if(book.bookID==this.getBookID()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public int getNumber() {
+        return number;
+    }
 
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
+    public String getBookID() {
+        return bookID;
+    }
 
+    public String getISBN() {
+        return ISBN;
+    }
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getTranslator() {
+        return translator;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setTranslator(String translator) {
+        this.translator = translator;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookID='" + bookID + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                ", bookName='" + bookName + '\'' +
+                ", author=" + author +
+                ", translator='" + translator + '\'' +
+                ", press='" + press + '\'' +
+                ", price=" + price +
+                ", money='" + money + '\'' +
+                ", number=" + number +
+                '}';
+    }
 }
