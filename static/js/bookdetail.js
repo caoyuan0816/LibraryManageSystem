c71584caac6215a3708d6a933f1c9ec2ba18bbde
superpig <940578704@qq.com>
2015-05-30 01:37:51 +0800

    two ajax

---
+++ static/js/bookdetail.js
@@ -0,0 +1,40 @@
+//get param from url
+function getUrlParam(name){  
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");  
+    var r = window.location.search.substr(1).match(reg);  
+    if (r!=null) return unescape(r[2]);  
+    return null;  
+}
+$(function(){
+	var bookid = getUrlParam('bookid');
+	$.post('/api/book-detail/', {bookid: bookid}, function(data) {
+		/*optional stuff to do after success */
+		if (data.status) {
+			$('.book-content').empty();
+			var bookDetailDom = '<div class="row">'
+		              +'<div class="col-sm-6 book-pho">'
+		                +'<a href="' + data["book"].photoURL + '">'
+		                  +'<img src="'+ data["book"].photoURL  +'" alt=""/>'
+		                +'</a>'
+		              +'</div>'
+		              +'<div class="col-sm-6 book-inf">'
+		                +'<p><span>书名：</span><span>'+ data["book"].bookName + '</span></p>'
+		                +'<p><span>作者：</span><span>'+data["book"].author + '</span></p>'
+		                +'<p><span>出版社：</span><span>'+data["book"].publisher + '</span></p>'
+		                +'<p><span>出版时间：</span><span>'+data["book"].publishTime + '</span></p>'
+		                +'<p><span>ＩＳＢＮ码：</span><span>'+data["book"].isbn + '</span></p>'
+		                +'<p><span>译者：</span><span>'+data["book"].translator + '</span></p>'
+		                +'<p><span>现存量：</span><span>'+data["book"].currentStorage + '</span></p>'
+		                +'<p>作者介绍：</p>'
+		                +'<p>'+data["book"].authorIntroduction + '</p>'
+		              +'</div>'
+		            +'</div>'
+		            +'<div class="row book-intr">'
+		              +'<h4>内容简介：</h4>'
+		              +'<pre>' +data["book"].bookIntroduction 
+		              +'</pre>'
+		            +'</div>';
+			$('.book-content').append(bookDetailDom);
+		}
+	});
+});
--- static/js/booklist.js
+++ static/js/booklist.js
@@ -1,21 +1,45 @@
 // book search page script 
-$(function() {
-	$.post('/api/book-search/', {page: '1'}, function (data) {
+function freshBookList(pageNumber) {
+	$.post('/api/book-search/', {page: pageNumber}, function (data) {
 		console.log('book-search');
 		console.log(data);
-            if (data.status) {
-            	$('.book-dis').empty();
-                for (var i =0 ; i < data.length; i++) {
-                	var bookDom = '<li>'
-					               	+'<a href="">'
-					               	+'<img src="/static/images/bk2.jpg" alt=""/>'
-					               	+'<p>' +data[i].booName+'</p>'
-					                	+'</a>'
-					              + '</li>';
-			$('.book-dis').append(bookDom); 
-               }	
+            	if (data.status) {
+            		$('.book-dis').empty();
+	              	for (var i =0 ; i < data.length; i++) {
+	              		var bookDom = '<li>'
+						               	+'<a href="/book-detail/bookid=' +data[i].id +'">'
+						               	+'<img src="' + data[i].photoURL +'" alt=""/>'
+						               	+'<p>' +data[i].booName+'</p>'
+						                	+'</a>'
+					              		+ '</li>';
+				$('.book-dis').append(bookDom); 
+             		}
+	             	var booksPerPage = 10;
+	             	var all_number = data.all_number;
+	             	var pages = all_number / booksPerPage;
+	             	if (all_number % booksPerPage != 0) {
+	             		pages ++;
+	             	}
+	             	pages = 10;
+	             	$('.pagination').empty();
+	             	for (var i = 1; i <= pages; i++) {
+	             		var pgDom = '<li>'
+	             						+'<span>' + i + '</span>'
+	             					+'</li>';
+	             		$('.pagination').append(pgDom);
+	             	}
             } else {
                alert("hello");
             }
         });
+}
+$(function() {
+	freshBookList(1);
+	$('.pagination').on('click', 'span', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		alert( $( this ).text() );
+		var page = parseInt($(this).val());
+		freshBookList(page);
+	});
 });
