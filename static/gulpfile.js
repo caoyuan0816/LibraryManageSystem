3fa6b7db5ab9bcaa9b70c5b1f19c3309f1094a75
superpig <940578704@qq.com>
2015-06-03 21:15:07 +0800

    change path

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -7,6 +7,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
 
 //check script
 gulp.task('lint', function() {
@@ -22,18 +23,22 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('./dist/css'));
 });
 
-//combine and compass file
-gulp.task('scripts', function(){
+//combine and compress file
+gulp.task('compress', function(){
 	gulp.src('./src/ourjs/*.js')
 		.pipe(concat('main.js'))
 		.pipe(gulp.dest('./dist/js'))
 		.pipe(rename('main.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('./dist/js'));
+	gulp.src('./dist/css/*.css')
+		.pipe(rename('index.min.css'))
+		.pipe(minifyCss({compatibility: 'ie8'}))
+		.pipe(gulp.dest('./dist/css'));
 });
 //default task
 gulp.task('default', function(){
-    gulp.run('lint', 'sass', 'scripts');
+    gulp.run('lint', 'sass', 'compress');
 
     // monitor change
     gulp.watch('./src/js/*.js', function(){
