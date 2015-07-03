3e7313118a06ed801537ab556f91b27d69d4f177
hy931006 <414710751@qq.com>
2015-07-03 21:54:37 +0800

    added single fine api

---
+++ src/main/java/hello/controller/APISingleFineController.java
@@ -0,0 +1,65 @@
+package hello.controller;
+
+import hello.model.Account;
+import hello.model.AccountRepository;
+import hello.model.Record;
+import hello.model.RecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * *
+ *
+ * @author yang
+ * @modify 2015/7/321:47
+ */
+@RestController
+@RequestMapping("/api/single-fine/")
+public class APISingleFineController {
+    @Autowired
+    RecordRepository recordRepository;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public double post(@RequestParam(value="username",defaultValue = "")String username,
+                       @RequestParam(value="id",defaultValue = "")String bookid){
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!username.equals(userDetails.getUsername())) {
+            return -1;
+        }
+        boolean is_Staff = false;
+
+        for(GrantedAuthority s : userDetails.getAuthorities()){
+            if (s.getAuthority().equals("ROLE_STAFF")) {
+                is_Staff = true;
+            }
+        }
+
+        if (!is_Staff){
+            return -1;
+        }
+        if(username.equals(""))
+            return -1;
+        double fine = -1;
+        Account accout = accountRepository.findByUsername(username);
+        Record record = recordRepository.findByUseridAndBookid(accout.getId(), bookid);
+        if(System.currentTimeMillis()>record.getReturntime()&&record.getActualreturntime()==-1){
+            fine = (System.currentTimeMillis()-record.getReturntime())/(1000*60*60*24)*0.5;
+        }
+        return fine;
+    }
+}
